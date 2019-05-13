5147da7af099a50deb5a988f7dc57f6769e79eee
Flo B Jones <flocela@gmail.com>
2019-05-13 17:00:29 -0500

    added validation testing to Song model, also added Song model class.

---
+++ app/models/song.rb
@@ -0,0 +1,14 @@
+class Song < ActiveRecord::Base
+  validates :new_work_title, presence: true 
+  validates :song_type, presence: true
+  validates :orig_english_title, presence: true
+  validates :orig_artist, presence: true
+  validates :orig_downloaded_at_name, presence: true
+  validates :orig_downloaded_at_link, presence: true
+  validates :orig_license_name, presence: true
+  validates :orig_license_link, presence: true
+  validates :name_of_new_work_license, presence: true
+  validates :link_to_new_work_license, presence: true
+  validates :filename, presence: true
+end
+
