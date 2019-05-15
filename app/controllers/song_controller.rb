c244412dc73d21b01d545c96664707c0ec9a3f42
Flo B Jones <flocela@gmail.com>
2019-05-15 16:31:23 -0500

    Created request/songs_spec.rb. It tests that all songs in database are returned (json) when calling GET /songs. In order to do this, I installed factorybot, I added song_controller.rb, I added the GET /songs route to routes.rb.

---
+++ app/controllers/song_controller.rb
@@ -0,0 +1,7 @@
+class SongController < ApplicationController
+  def index
+    songs = Song.all
+    render(json: {'songs' => songs.map{ |song| song[:new_work_title]}})  
+  end
+end
+
---
+++ spec/factories.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :song do
+    new_work_title {"new work title"}
+    song_type {"1"}
+    orig_english_title {"orig english title"}
+    orig_artist {"orig_artist"}
+    orig_downloaded_at_name {"orig downloaded at name"}
+    orig_downloaded_at_link {"orig downloaded at link"}
+    orig_license_name {"orig license name"}
+    orig_license_link {"orig license link"}
+    name_of_new_work_license {"name of new work license"}
+    link_to_new_work_license {"link of new work license"}
+    filename {"filename"}
+  end
+end
