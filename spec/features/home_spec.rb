d38805ed70c2f025cb0b34f1b038198276e1feeb
Flo B Jones <flocela@gmail.com>
2019-05-15 21:55:22 -0500

    Added home page with a link to song/index page. Added view to songs/index

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -5,5 +5,11 @@ RSpec.describe 'Home features' do
     visit('/home')
     expect(page).to have_content('Cantando Ingles')
   end
+  it 'displays link to songs page' do
+    visit('/home')
+    click_link('See All Songs')
+    expect(current_path).to eql('/songs')
+    expect(page).to have_content('<h2>Songs</Songs>')
+  end
 end
 
