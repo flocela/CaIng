310400b176794f098f09c4824a51ea8918635533
Flo B Jones <flocela@gmail.com>
2019-05-30 17:04:16 -0500

    admin/songs: Have added check to make sure user is me through email verification

--- app/controllers/admin/songs_controller.rb
+++ app/controllers/admin/songs_controller.rb
@@ -1,32 +1,42 @@
 class Admin::SongsController < ApplicationController
   before_action :authenticate_admin!
   def index
-    unless current_admin
-      redirect_to :back, :alert => "Access denied."
+    puts current_admin.email
+    puts Rails.application.credentials.admin_email
+    if current_admin && current_admin.email == 'flocela@gmail.com'
+      @songs = Song.all
+    else
+      redirect_back(fallback_location: admin_session_path, alert: "Access denied.")
+      #redirect_to :back, :alert => "Access denied."
     end
-    @songs = Song.all
   end
   def new
-    unless current_admin
-      redirect_to :back, :alert => "Access denied."
+    if current_admin && current_admin.email == 'flocela@gmail.com'
+      @songs = Song.all
+      @song = Song.new()
+      #redirect_to :back, :alert => "Access denied."
+    else
+      redirect_back(fallback_location: admin_session_path, method: :delete, alert: "Access denied.")
     end
-   @song = Song.new()
   end
   def create
-    unless current_admin
-      redirect_to :back, :alert => "Access denied."
+    if current_admin && current_admin.email == 'flocela@gmail.com'
+      song = Song.new(song_params)
+      song.save!
+      redirect_to(admin_songs_path)
+    else
+      redirect_back(fallback_location: admin_session_path, method: :delete, alert: "Access denied.")
     end
-    song = Song.new(song_params)
-    song.save!
-    redirect_to(admin_songs_path)
   end
   def destroy
-    unless current_admin
-      redirect_to :back, :alert => "Access denied."
+    if current_admin && current_admin.email == 'flocela@gmail.com'
+      song = Song.new(song_params)
+      song = Song.find(params[:id])
+      song.destroy
+      redirect_to admin_songs_path
+    else
+      redirect_back(fallback_location: admin_session_path, method: :delete, alert: "Access denied.")
     end
-    song = Song.find(params[:id])
-    song.destroy
-    redirect_to admin_songs_path
   end  
   def song_params
     params.require(:song).permit(:new_work_title, :song_type, :orig_english_title, :orig_artist, :orig_downloaded_at_name, :orig_downloaded_at_link, :orig_license_name, :orig_license_link, :name_of_new_work_license, :link_to_new_work_license, :filename)
