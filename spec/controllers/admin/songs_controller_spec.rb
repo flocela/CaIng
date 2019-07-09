5d980d19ae2985a66fc2ffc6c1b6207f1a5e1f8d
Flo B Jones <flocela@gmail.com>
2019-07-09 16:16:04 -0500

    spec/controllers/admin/songs_controller_spec.rb: Testing if an admin other than flocela can delete and create songs. Can not.

--- spec/controllers/admin/songs_controller_spec.rb
+++ spec/controllers/admin/songs_controller_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Admin::SongsController do 
 # SongsController defines the controller. This test must be inside /controllers or /requests directories. Maybe, file also must be named songs_controller.
-  it 'deletes a song from the list of songs' do
+ it 'flocela admin deletes a song from the list of songs' do
     admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
     sign_in admin
     song1 = create("song", new_work_title: 'new work title 1')
@@ -12,7 +12,7 @@ RSpec.describe Admin::SongsController do
     expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(-1)
   end
 
-  it 'does not allow an unconfirmed admin to delete a song' do
+  it 'does not allow song deletion when admin is signed out' do
     admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
     sign_in admin
     song1 = create("song", new_work_title: 'new work title 1')
@@ -22,8 +22,21 @@ RSpec.describe Admin::SongsController do
     sign_out admin
     expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(0)
   end
+  
+  it 'does not allow song deletion when admin is other than flocela' do
+    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+    sign_in admin
+    song1 = create("song", new_work_title: 'new work title 1')
+    expect(Song.count).to eq(1)
+    create("song", new_work_title: 'new work title 2')
+    expect(Song.count).to eq(2)
+    sign_out admin
+    not_flo_admin = Admin.create!(email: "abc@gmail.com", password: "secret", password_confirmation: "secret")
+    sign_in not_flo_admin #note: sign_in signs up abc@gmail.com although only flocela is allowed to register.
+    expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(0)
+  end
 
-  it 'creates a song when admin is signed in' do
+  it 'flocela admin creates a song' do
     song_attributes = attributes_for(:song)
     song_attributes[:new_work_title].should eq('new work title')
     admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
@@ -34,7 +47,14 @@ RSpec.describe Admin::SongsController do
   it 'does not create a song when admin is not signed in' do
     song_attributes = attributes_for(:song)
     song_attributes[:new_work_title].should eq('new work title')
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+    expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(0)
+  end
+
+  it 'does not create a song when admin signed in is other than flocela ' do
+    song_attributes = attributes_for(:song)
+    song_attributes[:new_work_title].should eq('new work title')
+    admin = Admin.create!(email: "other@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+    sign_in admin
     expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(0)
   end
 end
