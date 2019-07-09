d537a34647acb7d99e1cbaee37253132c9d483e6
Flo B Jones <flocela@gmail.com>
2019-07-09 17:41:36 -0500

    testing home page: Click to See All Songs goes to /songs instead of /admin/song

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'Home features' do
+RSpec.describe 'features/home: ' do
   it 'displays the name of the app' do
     visit('/home')
     expect(page).to have_content('Cantando Ingles')
@@ -8,7 +8,7 @@ RSpec.describe 'Home features' do
   it 'displays link to songs page' do
     visit('/home')
     click_link('See All Songs')
-    expect(current_path).to eql('/admin/songs')
+    expect(current_path).to eql('/songs')
     expect(page).to have_content('Songs')
   end
 end
