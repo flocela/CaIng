c407bbcc5838c51b6a369fff0b0ce59b71de3d09
Flo B Jones <flocela@gmail.com>
2019-07-10 15:50:14 -0500

    spec/features/songs_spec.rb deleted create a song and delete a song test, since must be in admin/songs, be an admin, to crete or delete a song

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -10,36 +10,4 @@ RSpec.describe 'Songs features' do
       expect(page).to have_content('new work title 2')
     end
   end
-=begin 
-  SAVING THESE TESTS FOR ADMIN/SONGS IN CASE I CAN EVER REGISTER AND SIGN IN ONCE FOR ALL TESTS IN A FEATURES TEST.
-describe 'adding a new song' do
-    it 'adds the song to the list of songs' do
-      visit('/admin/songs/new')
-      fill_in('New Work Title', with: 'New Work Title 3')
-      fill_in('Song Type', with: '3')
-      fill_in('Original Title in E', with: 'Orig Eng Title 3')
-      fill_in('Original Artist', with: 'Orig Artist 3')
-      fill_in('Originally Downloaded At Name', with: 'Orig Downloaded At Name 3')
-      fill_in('Originally Downloaded At Link', with: 'Orig Downloaded At Link 3')
-      fill_in('Original License Name', with: 'Orig License Name 3')
-      fill_in('Original License Link', with: 'Orig License Link 3')
-      fill_in('New License Name', with: 'Name of New Work License 3') 
-      fill_in('New License Link', with: 'Link to New Work License 3')
-      fill_in('Filename', with: 'Filename 3')
-      click_button('Create')
-     
-      expect(current_path).to have_content('/admin/songs')
-      expect(page).to have_content('New Work Title 3')
-    end
-  end
-  describe 'deleting a song' do
-    it 'deletes a song' do
-      create("song", new_work_title: 'new work title 1')
-      create("song", new_work_title: 'new work title 2')
-      visit('/admin/songs')
-      expect(page).to have_content('Delete')
-      click_link('Delete', match: :first)
-    end
-  end
-=end
 end
