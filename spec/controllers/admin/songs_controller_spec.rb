a66663bb207a31cc090e67c3b3e8385d1d0d3a18
Flo B Jones <flocela@gmail.com>
2019-07-17 16:24:02 -0500

    admin/songs_controller_spec: organized tests using context blocks, flocela signed in, other than flocela signed in, no one signed in.

--- spec/controllers/admin/songs_controller_spec.rb
+++ spec/controllers/admin/songs_controller_spec.rb
@@ -2,131 +2,147 @@ require 'rails_helper'
 
 RSpec.describe Admin::SongsController do 
 # SongsController defines the controller. This test must be inside /controllers or /requests directories. Maybe, file also must be named songs_controller.
-  it 'allows flocela admin to open admin/songs/index' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    get :index
-    expect(response).to render_template("index")
-  end 
- 
-  it 'does not allow admin other than flocela to open admin/songs/index' do
-    admin = Admin.create!(email: "abc@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    get :index
-    expect(response).to redirect_to(new_admin_session_path)
+  describe 'flocela signs in as admin and ' do
+    before(:each) do
+      admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+      sign_in admin
+    end
+    
+    it 'is allowed to open admin/songs/index' do
+      get :index
+      expect(response).to render_template("index")
+    end 
+    
+    it 'is allowed to open admin/songs/new' do
+      get :new
+      expect(response).to render_template("new")
+    end 
+    
+    it 'is allowed to open admin/songs/edit' do
+      song = create("song", new_work_title: 'new work title1')
+      get :edit, params: {id: song.id}
+      expect(response).to render_template("edit")
+    end 
+  
+    it 'is allowed to create a song' do
+      song_attributes = attributes_for(:song)
+      song_attributes[:new_work_title].should eq('new work title')
+      expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(1)
+    end
+  
+    it 'is allowed to update a song' do
+      song1 = create("song", new_work_title: 'new work title 1', song_type: '1')
+      expect(Song.count).to eq(1)
+      create("song", new_work_title: 'new work title 2')
+      expect(Song.count).to eq(2)
+      put :update, params: {id: song1.id, song: {new_work_title: "changed title"}}
+      expect(Song.find(song1.id).new_work_title).to eql("changed title")    
+      expect(Song.find(song1.id).song_type).to eql(1)
+    end
+
+    it 'is allowed to delete a song from the list of songs' do
+      song1 = create("song", new_work_title: 'new work title 1')
+      expect(Song.count).to eq(1)
+      create("song", new_work_title: 'new work title 2')
+      expect(Song.count).to eq(2)
+      expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(-1)
+    end
+  
   end
   
-  it 'allows flocela admin to open admin/songs/edit' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    song = create("song", new_work_title: 'new work title1')
-    get :edit, params: {id: song.id}
-    expect(response).to render_template("edit")
-  end 
-
-  it "does not allow admin other than flocela to open admin/songs/edit" do
-    admin = Admin.create!(email: "abc@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    song = create("song", new_work_title: 'new work title1')
-    get :edit, params: {id: 1}
-    expect(response).to redirect_to(new_admin_session_path)
-  end
-
-  it 'allows flocela admin to open admin/songs/new' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    get :new
-    expect(response).to render_template("new")
-  end 
+  describe 'Admin other than flocela signs in and ' do
+    before(:each) do
+      admin = Admin.create!(email: "abc@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+      sign_in admin
+    end
   
-  it 'does not allow other than flocela admin to open admin/songs/new' do
-    admin = Admin.create!(email: "abc@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    get :new
-    expect(response).to redirect_to(new_admin_session_path)
-  end 
+    it 'is not allowed to open admin/songs/index' do
+      get :index
+      expect(response).to redirect_to(new_admin_session_path)
+    end
+    
+   it 'is not allowed to open admin/songs/new' do
+      get :new
+      expect(response).to redirect_to(new_admin_session_path)
+    end 
+    
+    it "is not allowed to open admin/songs/edit" do
+      song = create("song", new_work_title: 'new work title1')
+      get :edit, params: {id: 1}
+      expect(response).to redirect_to(new_admin_session_path)
+    end
 
-  it 'allows flocela admin to update a song' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    song1 = create("song", new_work_title: 'new work title 1', song_type: '1')
-    expect(Song.count).to eq(1)
-    create("song", new_work_title: 'new work title 2')
-    expect(Song.count).to eq(2)
-    put :update, params: {id: song1.id, song: {new_work_title: "changed title"}}
-    expect(Song.find(song1.id).new_work_title).to eql("changed title")    
-    expect(Song.find(song1.id).song_type).to eql(1)
-  end
-
-  it 'does not allow admin other than flocela to update a song' do
-    admin_flocela = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin_flocela
-    song1 = create("song", new_work_title: 'new work title 1', song_type: '1')
-    expect(Song.count).to eq(1)
-    create("song", new_work_title: 'new work title 2')
-    expect(Song.count).to eq(2)
-    sign_out admin_flocela
-    admin = Admin.create!(email: "abc@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    put :update, params: {id: song1.id, song: {new_work_title: "changed title"}}
-    expect(Song.find(song1.id).new_work_title).to eql("new work title 1")    
-    expect(Song.find(song1.id).song_type).to eql(1)
-  end
-
-
-  it 'flocela admin deletes a song from the list of songs' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    song1 = create("song", new_work_title: 'new work title 1')
-    expect(Song.count).to eq(1)
-    create("song", new_work_title: 'new work title 2')
-    expect(Song.count).to eq(2)
-    expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(-1)
-  end
-
-  it 'does not allow song deletion when admin is signed out' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    song1 = create("song", new_work_title: 'new work title 1')
-    expect(Song.count).to eq(1)
-    create("song", new_work_title: 'new work title 2')
-    expect(Song.count).to eq(2)
-    sign_out admin
-    expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(0)
-  end
+    it 'does not create a song when admin signed in is other than flocela ' do
+      song_attributes = attributes_for(:song)
+      song_attributes[:new_work_title].should eq('new work title')
+      admin = Admin.create!(email: "other@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+      sign_in admin
+      expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(0)
+    end
+    
+    it 'does not update a song' do
+      song1 = create("song", new_work_title: 'new work title 1', song_type: '1')
+      expect(Song.count).to eq(1)
+      create("song", new_work_title: 'new work title 2')
+      expect(Song.count).to eq(2)
+      put :update, params: {id: song1.id, song: {new_work_title: "changed title"}}
+      expect(Song.find(song1.id).new_work_title).to eql("new work title 1")    
+      expect(Song.find(song1.id).song_type).to eql(1)
+    end
   
-  it 'does not allow song deletion when admin is other than flocela' do
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    song1 = create("song", new_work_title: 'new work title 1')
-    expect(Song.count).to eq(1)
-    create("song", new_work_title: 'new work title 2')
-    expect(Song.count).to eq(2)
-    sign_out admin
-    not_flo_admin = Admin.create!(email: "abc@gmail.com", password: "secret", password_confirmation: "secret")
-    sign_in not_flo_admin #note: sign_in signs up abc@gmail.com although only flocela is allowed to register.
-    expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(0)
+    it 'does not delete a song' do
+      song1 = create("song", new_work_title: 'new work title 1')
+      expect(Song.count).to eq(1)
+      create("song", new_work_title: 'new work title 2')
+      expect(Song.count).to eq(2)
+      expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(0)
+    end
+
   end
 
-  it 'flocela admin creates a song' do
-    song_attributes = attributes_for(:song)
-    song_attributes[:new_work_title].should eq('new work title')
-    admin = Admin.create!(email: "flocela@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(1)
-  end
+  describe 'No admin signs in and' do
+    
+    it 'opening admin/songs/index is not allowed' do
+      get :index
+      expect(response).to redirect_to(new_admin_session_path)
+    end
+    
+    it 'opening admin/songs/new is not allowed' do
+      get :new
+      expect(response).to redirect_to(new_admin_session_path)
+    end 
+    
+    it "opening admin/songs/edit is not allowed" do
+      song = create("song", new_work_title: 'new work title1')
+      get :edit, params: {id: 1}
+      expect(response).to redirect_to(new_admin_session_path)
+    end
 
-  it 'does not create a song when admin is not signed in' do
-    song_attributes = attributes_for(:song)
-    song_attributes[:new_work_title].should eq('new work title')
-    expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(0)
-  end
+    it 'creating a song is not allowed' do
+      song_attributes = attributes_for(:song)
+      song_attributes[:new_work_title].should eq('new work title')
+      admin = Admin.create!(email: "other@gmail.com", password: "very-secret", password_confirmation: "very-secret")
+      sign_in admin
+      expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(0)
+    end
+    
+    it 'updating a song is not allowed' do
+      song1 = create("song", new_work_title: 'new work title 1', song_type: '1')
+      expect(Song.count).to eq(1)
+      create("song", new_work_title: 'new work title 2')
+      expect(Song.count).to eq(2)
+      put :update, params: {id: song1.id, song: {new_work_title: "changed title"}}
+      expect(Song.find(song1.id).new_work_title).to eql("new work title 1")    
+      expect(Song.find(song1.id).song_type).to eql(1)
+    end
+  
+    it 'deleting a song is not allowed' do
+      song1 = create("song", new_work_title: 'new work title 1')
+      expect(Song.count).to eq(1)
+      create("song", new_work_title: 'new work title 2')
+      expect(Song.count).to eq(2)
+      expect { delete :destroy, params: { id: song1.id } }.to change(Song, :count).by(0)
+    end
 
-  it 'does not create a song when admin signed in is other than flocela ' do
-    song_attributes = attributes_for(:song)
-    song_attributes[:new_work_title].should eq('new work title')
-    admin = Admin.create!(email: "other@gmail.com", password: "very-secret", password_confirmation: "very-secret")
-    sign_in admin
-    expect { post(:create, params: { song: song_attributes}) }.to change(Song, :count).by(0)
-  end
+  end  
 end
