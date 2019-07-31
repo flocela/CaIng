c8bafe80ee4b6f7e365bea58f7ae3de53aeab565
Flo B Jones <flocela@gmail.com>
2019-07-31 11:17:09 -0500

    songs/get_zip/:id testing continuing. Now tests that correct file is sent, based on id

--- spec/features/songs_spec.rb
+++ spec/features/songs_spec.rb
@@ -9,5 +9,10 @@ RSpec.describe 'Songs features' do
       expect(page).to have_content('new work title 1')
       expect(page).to have_content('new work title 2')
     end
+    it 'downloads zip file from amazon' do
+      create("song", new_work_title: 'amazing_grace_download', id: 15, filename: 'amazing_grace_rich_tuttle.zip')
+      expect_any_instance_of(SongsController).to receive(:send_file).with("app/assets/songs/amazing_grace_rich_tuttle.zip", :filename => "amazing_grace_rich_tuttle.zip", :url_based_filename => false, :type => "application/zip").and_call_original
+      visit('/songs/get_zip/15')
+    end
   end
 end
