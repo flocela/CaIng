93029014f78723adfd09eb0a9396fda0fb86d067
Flo B Jones <flocela@gmail.com>
2019-08-29 10:05:36 -0500

    fixed testing, since changed name of links.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -3,11 +3,12 @@ require 'rails_helper'
 RSpec.describe 'features/home: ' do
   it 'displays the name of the app' do
     visit('/home')
-    expect(page).to have_content('Cantando Ingles')
+    expect(page).to have_content('Cantando Inglés')
   end
+
   it 'displays link to songs page' do
     visit('/home')
-    click_link('See All Songs')
+    click_link('All Songs')
     expect(current_path).to eql('/songs')
     expect(page).to have_content('Songs')
   end
