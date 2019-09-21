bbacf577d3b08a4a6991290c6bba29b65f017a39
Flo B Jones <flocela@gmail.com>
2019-09-20 21:19:40 -0500

    home page: added spanish header.

--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -1,18 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe 'Home Feature Spec' do
-  it 'displays the name of the app' do
-    visit('/home')
-    expect(page).to have_content('Cantando Inglés')
-  end
-
-  it 'displays link to songs page' do
-    visit('/home')
-    within '.top-menu' do
-      click_link('Songs')
+  describe 'en home' do
+    it 'displays the name of the app' do
+      visit('/en/home')
+      expect(page).to have_content('Cantando Inglés')
     end
-    expect(current_path).to eql('/songs')
-    expect(page).to have_content('Songs')
+    
+    it 'displays link to songs page' do
+      visit('/en/home')
+      within '.top-menu' do
+	click_link('Songs')
+      end
+      expect(current_path).to eql('/en/songs')
+      expect(page).to have_content('Songs')
+    end
   end
+ 
+  describe 'es home' do
+    it 'displays the name of the app' do
+      visit('/home')
+      expect(page).to have_content('Cantando Inglés')
+    end
+
+    it 'displays link to songs page' do
+      visit('/home')
+      within '.top-menu' do
+	click_link('Canciones')
+      end
+      expect(current_path).to eql('/es/songs')
+      expect(page).to have_content('Canciones')
+    end
+  end 
 end
 
