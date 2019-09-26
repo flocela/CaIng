dd0967303d5ecf0aea93c2a9507040b9f7b6dd3e
Flo B Jones <flocela@gmail.com>
2019-09-26 14:52:40 -0500

    Added header test for english/spanish toggle in spec/features/inicio_spec.rb

--- spec/features/inicio_spec.rb
+++ spec/features/inicio_spec.rb
@@ -2,17 +2,26 @@ require 'rails_helper'
 
 RSpec.describe 'Canciones Feature Spec' do
   it 'displays the name of the app' do
-    visit('/es/home')
+    visit('/home')
     expect(page).to have_content('Cantando Inglés')
   end
 
   it 'displays link to canciones page' do
-    visit('/es/home')
+    visit('/home')
     within '.top-menu' do
       click_link('Canciones')
     end
     expect(current_path).to eql('/es/songs')
     expect(page).to have_content('Canciones')
+  end
+  
+  it 'click on English goes to English page' do
+    visit('/home')
+    within '.menu-flex-container-sm .link-espanol' do
+      click_link('English')
+    end
+    expect(current_path).to eq '/en/home'
+    expect(page).to have_content('This phone application') 
   end
 end
 
