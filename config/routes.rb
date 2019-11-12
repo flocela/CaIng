9f418ad3f9ab1494a69780062528e549050658c9
Flo B Jones <flocela@gmail.com>
2019-11-11 18:20:38 -0600

    Changes in application_controller.rb and config/application.rb and config/routes.rb that are required to get I18N working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :posts
-  root 'welcome#index'
+  root 'home#index'
+  scope "(:locale)", locale: /en|es/ do
+    get('home' => 'home#index')
+    get('songs' => 'songs#index')
+  end
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
