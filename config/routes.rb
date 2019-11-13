33f35061c93a34eabb4fe99ff6ac41350e655db3
Flo B Jones <flocela@gmail.com>
2019-11-13 13:12:23 -0600

    Devise: created Admin model and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins, controllers: {sessions: 'admins/sessions', registrations: 'admins/registrations'}
   resources :posts
   root to: 'home#index'
   scope "(:locale)", locale: /en|es/ do
