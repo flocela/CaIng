ed85c1070b71ecd352b8cb30922785c9a98cb80f
Flo B Jones <flocela@gmail.com>
2019-11-13 17:07:59 -0600

    added songs table to database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_13_190558) do
+ActiveRecord::Schema.define(version: 2019_11_13_225751) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,35 @@ ActiveRecord::Schema.define(version: 2019_11_13_190558) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "new_work_title", null: false
+    t.integer "song_type", null: false
+    t.string "orig_english_title", null: false
+    t.string "orig_artist", null: false
+    t.string "orig_downloaded_at_name", null: false
+    t.string "orig_downloaded_at_link", null: false
+    t.string "orig_pub_date"
+    t.string "orig_lyrics_date"
+    t.string "orig_lyrics_date_link_name"
+    t.string "orig_lyrics_date_link"
+    t.string "orig_license_name", null: false
+    t.string "orig_license_link", null: false
+    t.text "orig_attribution_link"
+    t.string "orig_uses_sample_from_link_name_1"
+    t.string "orig_uses_sample_from_link_1"
+    t.string "orig_english_uses_sample_from_1"
+    t.string "orig_spanish_uses_sample_from_1"
+    t.string "name_of_new_work_license", null: false
+    t.string "link_to_new_work_license", null: false
+    t.string "orig_english_changes_made", null: false
+    t.string "orig_spanish_changes_made", null: false
+    t.string "filename", null: false
+    t.string "zip_size", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["new_work_title"], name: "index_songs_on_new_work_title", unique: true
   end
 
 end
