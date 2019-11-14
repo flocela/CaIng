e111573d7d6ee7cfba277c64d33247f85fea410e
Flo B Jones <flocela@gmail.com>
2019-11-14 09:35:11 -0600

    views/admin/songs: Adding views to admin/songs.

---
+++ spec/controllers/admin/songs_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Admin::SongsController do 
+
+  describe 'flocela signs in as admin, and ' do
+
+    before(:each) do
+      admin = create('admin', email: 'flocela@gmail.com')
+      sign_in admin
+    end
+
+    it 'is allowed to open admin/songs/index' do
+      get :index
+      expect(response).to render_template("index")
+    end 
+    
+    it 'is allowed to open admin/songs/new' do
+      get :new
+      expect(response).to render_template("new")
+    end 
+
+  end
+
+end
