8b201ac2eb98bf1aba5a516b59b0afc8ded040e1
Flo B Jones <flocela@gmail.com>
2019-11-14 10:53:12 -0600

    added spec/factories.rb, so I can create flocela admin, and generic song.

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,21 @@ FactoryBot.define do
     password_confirmation {"12341234"}
   end
 
+  factory :song do
+    new_work_title {"new work title"}
+    song_type {"1"}
+    orig_english_title {"orig english title"}
+    orig_artist {"orig_artist"}
+    orig_downloaded_at_name {"orig downloaded at name"}
+    orig_downloaded_at_link {"orig downloaded at link"}
+    orig_license_name {"orig license name"}
+    orig_license_link {"orig license link"}
+    name_of_new_work_license {"name of new work license"}
+    link_to_new_work_license {"link to new work license"}
+    orig_english_changes_made {"changes made in english"}
+    orig_spanish_changes_made {"changes made in spanish"}
+    filename {"filename"}
+    zip_size {5}
+  end
+
 end
