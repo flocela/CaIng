d66937c66cc7452eb8d6c10eec2c3969721c2b78
Flo B Jones <flocela@gmail.com>
2019-11-14 09:48:31 -0600

    admin/songs: Added routes for admin/songs. Added controller for admin/songs. Added view for new song, admin/songs/new.html.erb.

---
+++ app/controllers/admin/songs_controller.rb
@@ -0,0 +1,7 @@
+class Admin::SongsController < ApplicationController
+  
+  def new
+  end
+  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins, controllers: {sessions: 'admins/sessions', registrations: 'admins/registrations'}
   resources :posts
+  namespace :admin do
+    resources :songs
+  end
   root to: 'home#index'
   scope "(:locale)", locale: /en|es/ do
     get('home' => 'home#index')
