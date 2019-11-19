f8a4701c00fbf77402dcccb92f76684db2fc194c
Flo B Jones <flocela@gmail.com>
2019-11-19 11:16:30 -0600

    admin/home page: Added Admin/home page, including routing, controller, and view.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :songs
   end
+  get('admin/home' => 'admin/home#index')
   root to: 'home#index'
   scope "(:locale)", locale: /en|es/ do
     get('home' => 'home#index')
---
+++ spec/features/admin_home_spec.rb
@@ -0,0 +1,73 @@
+require 'rails_helper'
+describe 'Admin Home Feature Spec' do
+ 
+  describe 'flocela signs in as admin and ' do
+    before(:each) do
+      visit('/admins/sign_up')
+      fill_in('Email', with: 'flocela@gmail.com')
+      fill_in('Password', with: 'very-secret')
+      fill_in('Password confirmation', with: 'very-secret')
+      click_button('Sign up')
+    end
+
+    it 'Admin-Songs link is shown' do
+      visit('/admin/home')
+      within '.admin-links-column' do
+        expect(page).to have_link('Admin-Songs')
+      end
+    end
+    
+    it 'goes to admin/songs when Admin-Songs is clicked' do 
+      expect(current_path).to eql('/')
+      visit('/admin/home')
+      within '.admin-links-column' do
+	click_link('Admin-Songs')
+      end
+      expect(current_path).to eql('/admin/songs')
+    end
+    
+    it 'Sign-Out link is shown' do
+      visit('/admin/home')
+      expect(page).to have_link'Sign Out'
+    end
+
+    it 'Delete Registration link is shown' do
+      visit('/admin/home')
+      expect(page).to have_link'Delete Registration'
+    end
+    
+  end
+  
+  context "Admin is not signed in then" do
+    it 'Admin-Songs link is not shown' do
+      visit('/admin/home')
+      expect(page).to_not have_link('Admin-Songs')
+    end
+    
+    it 'Sign Out link is not shown' do
+      visit('/admin/home')
+      expect(page).to_not have_link('Sign Out')
+    end
+
+    it 'Delete Registration link is not shown' do
+      visit('/admin/home')
+      expect(page).to_not have_link('Delete Registration')
+    end
+    
+    it 'Sign In link is shown' do
+      expect(Admin.count).to equal(0)
+      visit('/admin/home')
+      expect(page).to have_content('Sign In')
+    end 
+    
+    it 'Register link is shown' do
+      expect(Admin.count).to equal(0)
+      visit('/admin/home')
+      expect(page).to have_content('Register')
+    end 
+
+    
+  end 
+
+
+end
