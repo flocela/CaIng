c71d63b6e382437c9b6b24e76313784189cedcca
Flo B Jones <flocela@gmail.com>
2019-11-19 10:36:12 -0600

    features/admin_registration_spec.rb: Added test showing non-flocela registrant can not register.

---
+++ spec/features/admin_registration_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+describe 'Admin Registration Feature Spec' do
+  
+  it 'will not register admin that is not flocela@gmail.com' do
+    visit('/admins/sign_up')
+    fill_in('Email', with: 'abc@gmail.com')
+    fill_in('Password', with: 'very-secret')
+    fill_in('Password confirmation', with: 'very-secret')
+    click_button('Sign up')
+    expect(Admin.count).to equal(0)
+  end
+
+end
+
